4dfb1c2dd8b4e92359d0f50096053685ec00cdc5
timonpitz <timon.pitz@web.de>
2019-10-16 17:25:00 +0200

    added Telegram Service and Controller

---
+++ backend/src/telegram/telegram.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+import { TelegramService } from './telegram.service';
+
+@Controller('telegram')
+export class TelegramController {
+    constructor(
+        private readonly telegramService: TelegramService
+    ) { }
+
+    @Get('/../')
+    public async test() {
+
+    }
+
+}
